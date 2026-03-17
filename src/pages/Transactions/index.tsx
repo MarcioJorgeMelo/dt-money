2ba56e22967c4e8cf384899cc9ad1a82e5a09852
Márcio Jorge <marcio.melo@livsaude.com.br>
2026-03-17 13:57:23 -0300

    feat: add transactions context on Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,12 @@
-/* eslint-disable react-hooks/set-state-in-effect */
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json()
-
-        setTransactions(data)
-    }
-
-    useEffect(() => {
-        loadTransactions()
-    }, [])
+    const { transactions } = useContext(TransactionsContext)
 
     return (
         <div>
