62d4659bcd638dab9488b4ebe39621dd95f9812e
Márcio Jorge <marcio.melo@livsaude.com.br>
2026-03-18 14:11:05 -0300

    feat: add useMemo to optimize variable re-rendering

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { TransactionsContext } from "../contexts/TransactionsContext"
 import { useContextSelector } from "use-context-selector"
 
@@ -7,26 +8,30 @@ export function useSummary() {
         return context.transactions
     })
 
+    // Com useMemo -> Fará a variavel ser renderizada apenas se 'transactions' mudar
+
     // Irei reduzir o array de objeto a isso:
     // { income: x, outcome: y, total: z }
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income') {
-                acc.income += transaction.price
-                acc.total += transaction.price
-            } else {
-                acc.outcome += transaction.price
-                acc.total -= transaction.price
-            }
+    const summary = useMemo(() => {
+        transactions.reduce(
+            (acc, transaction) => {
+                if (transaction.type === 'income') {
+                    acc.income += transaction.price
+                    acc.total += transaction.price
+                } else {
+                    acc.outcome += transaction.price
+                    acc.total -= transaction.price
+                }
 
-            return acc
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    )
+                return acc
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0
+            }
+        )
+    }, [transactions])
 
     return summary
 }
